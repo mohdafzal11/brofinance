edb771df5b2528306773b31a195c22bcf7cf868e
Mohd Afzal <afzalnaved0000@gmail.com>
2024-11-25 07:31:53 +0530

    a

---
+++ src/OpenGraph.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function OpenGraph() {
+  return (
+    <div>
+      <meta name="og:title" property="og:title" content="Test Title" />
+      <meta
+        name="og:description"
+        property="og:description"
+        content="Test Description"
+      />
+    </div>
+  );
+}
---
+++ src/hooks/usePage.jsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+
+export const usePage = ({
+  title,
+  description,
+  ogTitle,
+  ogDescription,
+  ogImage,
+  ogUrl,
+}) => {
+  useEffect(() => {
+    document.title = title;
+    setMetaTag("name", "description", description);
+    setMetaTag("property", "og:title", ogTitle || title || "");
+    setMetaTag("property", "og:description", ogDescription || description || "");
+    // setMetaTag("property", "og:image", ogImage || "");
+    setMetaTag("property", "og:url", ogUrl || window.location.href);
+
+    return () => {
+      document.title = "";
+      document.querySelector('[name="description"]').removeAttribute("content");
+    };
+  }, [title, description, ogTitle, ogDescription, ogImage, ogUrl]);
+
+  const setMetaTag = (attribute, key, content) => {
+    if (content) {
+      let element = document.querySelector(`meta[${attribute}="${key}"]`);
+      if (!element) {
+        element = document.createElement("meta");
+        element.setAttribute(attribute, key);
+        document.head.appendChild(element);
+      }
+      element.setAttribute("content", content);
+    }
+  };
+};
+
+export default usePage;
